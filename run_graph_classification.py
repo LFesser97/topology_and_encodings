2480e3eeffeaf628699ecd392ab91f6fc181db44
LFesser97 <53827774+LFesser97@users.noreply.github.com>
2024-01-10 05:29:16 -0500

    lcp for gin

--- run_graph_classification.py
+++ run_graph_classification.py
@@ -204,8 +204,8 @@ for key in datasets:
         # transform = T.RootedRWSubgraph(walk_length=10)
         # print("Encoding Rooted RW Subgraph")
 
-        transform = T.LocalDegreeProfile()
-        print("Encoding Local Degree Profile")
+        # transform = T.LocalDegreeProfile()
+        # print("Encoding Local Degree Profile")
 
         # transform = T.Compose([T.RootedRWSubgraph(walk_length=10), T.AddRandomWalkPE(walk_length=16)])
         # print("Encoding Rooted RW Subgraph + Random Walk PE")
@@ -213,11 +213,11 @@ for key in datasets:
         # transform = T.Compose([T.RootedRWSubgraph(walk_length=10), T.AddLaplacianEigenvectorPE(k=8)])
         # print("Encoding Rooted RW Subgraph + Laplacian Eigenvector PE")
       
-        # lcp = LocalCurvatureProfile()
-        # print(f"Encoding Local Curvature Profile (ORC) for graph {current_graph} of {org_dataset_len}")
+        lcp = LocalCurvatureProfile()
+        print(f"Encoding Local Curvature Profile (ORC) for graph {current_graph} of {org_dataset_len}")
 
-        # dataset[i] = lcp.compute_orc(dataset[i])
-        dataset[i] = transform(dataset[i])
+        dataset[i] = lcp.compute_orc(dataset[i])
+        # dataset[i] = transform(dataset[i])
 
         current_graph += 1
 
@@ -332,9 +332,9 @@ for key in datasets:
     run_duration = end - start
 
     # pickle the graph dictionary in a new file
-    with open(f"results/{key}_gin_ldp_graph_dict.pickle", "wb") as f:
+    with open(f"results/{key}_gin_lcp_graph_dict.pickle", "wb") as f:
         pickle.dump(graph_dict, f)
-        print(f"Graph dictionary for {key} with LDP pickled")
+        print(f"Graph dictionary for {key} with LCP pickled")
 
     train_mean = 100 * np.mean(train_accuracies)
     val_mean = 100 * np.mean(validation_accuracies)
