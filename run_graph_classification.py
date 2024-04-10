5090f8401f3a2b54b72f535720787010c5d843f0
LFesser97 <53827774+LFesser97@users.noreply.github.com>
2024-04-10 19:49:48 -0400

    100 trials

--- run_graph_classification.py
+++ run_graph_classification.py
@@ -133,7 +133,7 @@ default_args = AttrDict({
     "learning_rate": 1e-3,
     "layer_type": "R-GCN",
     "display": True,
-    "num_trials": 400,
+    "num_trials": 100,
     "eval_every": 1,
     "rewiring": None,
     "num_iterations": 1,
